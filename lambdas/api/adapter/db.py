ca1a7259e8119db5496ee8a83731af5ded379819
Jack Curran <jackcurran49@gmail.com>
2026-05-16 17:57:10 -0400

    update api

--- lambdas/api/adapter/db.py
+++ lambdas/api/adapter/db.py
@@ -6,7 +6,7 @@ import logging
 logger = logging.getLogger()
 
 
-def _get_connection_string() -> str:
+def get_connection_string() -> str:
     client = boto3.client("ssm", region_name=os.environ["AWS_REGION"])
     response = client.get_parameter(
         Name=os.environ["DB_CONNECTION_PARAMETER"], WithDecryption=True
@@ -14,14 +14,26 @@ def _get_connection_string() -> str:
     return response["Parameter"]["Value"]
 
 
-_conn = psycopg2.connect(_get_connection_string())
-
-
-def get_teams() -> list[dict]:
-    with _conn.cursor() as cur:
+def get_teams(conn: psycopg2.extensions.connection) -> list[dict]:
+    with conn.cursor() as cur:
         cur.execute(
             "SELECT team_id, name, abbreviation, city, league, division,"
             " venue_name, debut_year, active FROM teams ORDER BY name"
         )
         cols = [desc[0] for desc in cur.description]
         return [dict(zip(cols, row)) for row in cur.fetchall()]
+
+
+def get_player_info(conn: psycopg2.extensions.connection, player_id: int) -> dict:
+    with conn.cursor() as cur:
+        cur.execute(
+            "SELECT * FROM players WHERE player_id = %s",
+            (player_id,)
+        )
+        row = cur.fetchone()
+        if row:
+            cols = [desc[0] for desc in cur.description]
+            return dict(zip(cols, row))
+        else:
+            logger.warning(f"Player with id {player_id} not found")
+            return {}
--- lambdas/api/main.py
+++ lambdas/api/main.py
@@ -1,9 +1,13 @@
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
 from adapter import db
+import psycopg2
 
 logger = Logger()
 app = APIGatewayHttpResolver()
+
+db_string = db.get_connection_string()
+conn = psycopg2.connect(db_string)
 
 
 @app.get("/health")
@@ -13,7 +17,12 @@ def health():
 
 @app.get("/teams")
 def get_teams():
-    return {"teams": db.get_teams()}
+    return {"teams": db.get_teams(conn)}
+
+
+@app.get("/players/{player_id}/info")
+def get_player_info(player_id: int):
+    return {"player_id": player_id}
 
 
 @logger.inject_lambda_context
